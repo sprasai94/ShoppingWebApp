c6b89f718035a4d559f3f41b3de8c5bd9287ceb8
sprasai94 <46768706+sprasai94@users.noreply.github.com>
2020-11-13 10:38:57 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyD-U5-Gr0vvjT0sceFsGdIgXzDeaayhEzs",
-    authDomain: "oshop-a6929.firebaseapp.com",
-    databaseURL: "https://oshop-a6929.firebaseio.com",
-    projectId: "oshop-a6929",
-    storageBucket: "oshop-a6929.appspot.com",
-    messagingSenderId: "808727700207",
-    appId: "1:808727700207:web:8d4e8debec3dfed29ab11a",
-    measurementId: "G-QJQQF16KVS"
+   
   }
 };
